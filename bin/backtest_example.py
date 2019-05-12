dc39b6f4e84fc4cb03acb1636566a92776696223
Troy Stribling <troy.stribling@gmail.com>
2019-05-11 18:00:52 -0700

    started backtest example

--- bin/backtest_example.py
+++ bin/backtest_example.py
@@ -1,5 +1,8 @@
 import os
 import sys
+import backtrader as bt
+import alpaca_backtrader_api
+import pandas
 
 wd = os.getcwd()
 sys.path.append(wd)
@@ -7,3 +10,33 @@ sys.path.append(wd)
 from lib import alpaca
 
 credentials = alpaca.load_paper_credentials()
+
+class SmaCross(bt.SignalStrategy):
+    def __init__(self):
+        sma1, sma2 = bt.ind.SMA(period=10), bt.ind.SMA(period=30)
+        crossover = bt.ind.CrossOver(sma1, sma2)
+        self.signal_add(bt.SIGNAL_LONG, crossover)
+
+cerebro = bt.Cerebro()
+cerebro.addstrategy(SmaCross)
+
+store = alpaca_backtrader_api.AlpacaStore(key_id=credentials['key_id'],
+                                          secret_key=credentials['secret_key'],
+                                          paper=True)
+
+cerebro.broker.setcash(100000)
+cerebro.broker.setcommission(commission=0.0)
+cerebro.addsizer(bt.sizers.PercentSizer, percents=20)
+
+DataFactory = store.getdata
+
+data0 = DataFactory(dataname='AAPL',
+                    timeframe=bt.TimeFrame.TFrame("Minutes"),
+                    fromdate=pandas.Timestamp('2018-11-15'),
+                    todate=pandas.Timestamp('2018-11-17'),
+                    historical=True)
+cerebro.adddata(data0)
+
+cerebro.run()
+print('Final Portfolio Value: %.2f' % cerebro.broker.getvalue())
+cerebro.plot()
--- lib/alpaca.py
+++ lib/alpaca.py
@@ -4,6 +4,6 @@ import io
 
 def load_paper_credentials():
     credentials_file = os.path.join(os.environ["HOME"], ".alpaca/paper_trading_credentials.yml")
-    with io.open(config_file, "r") as stream:
+    with io.open(credentials_file, "r") as stream:
         credentials = yaml.safe_load(stream)
     return credentials
