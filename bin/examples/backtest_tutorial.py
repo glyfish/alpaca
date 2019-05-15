c0961d2da5c1b37de118bcd0a649532d0788ed19
Troy Stribling <troy.stribling@gmail.com>
2019-05-14 18:05:09 -0700

    added another tutorial step

--- bin/examples/backtest_tutorial.py
+++ bin/examples/backtest_tutorial.py
@@ -23,13 +23,45 @@ class TestStrategy(backtrader.Strategy):
 
     def __init__(self):
         self.dataclose = self.datas[0].close
+        self.order = None
         logger.info(f"Data source: {self.datas[0].p.dataname}")
 
     def next(self):
-        self.log('Close: %.2f, Open: %.2f, Volume: %d' % (self.dataclose[0], self.datas[0].open[0], self.datas[0].volume[0]))
-        if self.dataclose[0] < self.dataclose[-1] and self.dataclose[-1] < self.dataclose[-2]:
-            self.log('CREATE BUY ORDER: %.2f, %.2f, %.2f' % (self.dataclose[0], self.dataclose[-1], self.dataclose[-2]))
-            self.buy()
+        self.log('Close: %.2f' % (self.dataclose[0]))
+
+        # Do nothing if order is pending
+        if self.order:
+            return
+
+        # if not in market buy if criterion is met
+        if not self.position:
+            if self.dataclose[0] < self.dataclose[-1] and self.dataclose[-1] < self.dataclose[-2]:
+                self.log('CREATE BUY ORDER: %.2f, %.2f, %.2f' % (self.dataclose[0], self.dataclose[-1], self.dataclose[-2]))
+                self.order = self.buy()
+
+        # if in market sell if criterion is met
+        else:
+            # sell if positions has been held for more than 5 days
+            if len(self) >= (self.bar_executed + 5):
+                self.log('CREATE SELL ORDER: %.2f' % self.dataclose[0])
+                self.order = self.sell()
+
+
+    def notify_order(self, order):
+        if order.status in [order.Submitted, order.Accepted]:
+            return
+
+        if order.status in [order.Completed]:
+            if order.isbuy():
+                self.log('BUY COMPLETED: %.2f' % (order.executed.price))
+            elif order.issell():
+                self.log('SELL COMPLETED: %.2f' % (order.executed.price))
+
+            self.bar_executed = len(self)
+        elif order.status in [order.Canceled, order.Margin. order.Rejected]:
+            self.log('ORDER Cancelled/Margin/Rejected')
+
+        self.order = None
 
 # main
 def main():
