40b182cafce5d39ad69052cbb28ffcfdb791a39c
Troy Stribling <troy.stribling@gmail.com>
2019-05-22 18:57:04 -0700

    plot updates

--- hydrogen_notebooks/algorithmic_trading/random_walks.py
+++ hydrogen_notebooks/algorithmic_trading/random_walks.py
@@ -4,73 +4,26 @@
 
 import os
 import sys
-from datetime import datetime
-import backtrader
-from matplotlib import pyplot
-from lib import config
+from lib import brownian_motion
 import numpy
 
-pyplot.style.use(config.glyfish_style)
 wd = os.getcwd()
 yahoo_root = os.path.join(wd, 'data', 'yahoo')
 
 # %%
 
-def brownian_motion(Δt, n):
-    σ = numpy.sqrt(Δt)
-    samples = numpy.zeros(n)
-    for i in range(1, n):
-        Δ = numpy.random.normal()
-        samples[i] = samples[i-1] + σ * Δ
-    return samples
-
-def brownian_motion_with_drift(μ, σ, Δt, n):
-    samples = numpy.zeros(n)
-    for i in range(1, n):
-        Δ = numpy.random.normal()
-        samples[i] = samples[i-1] + (σ * Δ * numpy.sqrt(Δt)) + (μ * Δt)
-    return samples
-
-def geometric_brownian_motion(μ, σ, s0, Δt, n):
-    samples = brownian_motion_with_drift(μ, σ, Δt, n)
-    return s0*numpy.exp(samples)
-
-def multiplot(samples, time, text_pos, title, plot_name):
-    nplot = len(samples)
-    figure, axis = pyplot.subplots(figsize=(12, 8))
-    axis.set_xlabel("Time")
-    axis.set_ylabel("Value")
-    axis.set_title(title)
-    stats=f"Simulation Stats\n\nμ={format(numpy.mean(samples[:,-1]), '2.2f')}\nσ={format(numpy.std(samples[:,-1]), '2.2f')}"
-    bbox = dict(boxstyle='square,pad=1', facecolor="#FEFCEC", edgecolor="#FEFCEC", alpha=0.75)
-    axis.text(text_pos[0], text_pos[1], stats, fontsize=15, bbox=bbox)
-    for i in range(nplot):
-        axis.plot(time, samples[i], lw=1)
-    config.save_post_asset(figure, "mean_reversion", plot_name)
-
-def plot(samples, time, text_pos, plot_name):
-    nplot = len(samples)
-    figure, axis = pyplot.subplots(figsize=(12, 8))
-    axis.set_xlabel("Time")
-    axis.set_ylabel("Value")
-    axis.set_title(title)
-    axis.plot(time, samples, lw=1)
-    config.save_post_asset(figure, "mean_reversion", plot_name)
-
-# %%
-
 Δt = 0.01
 npts = 10000
 nsim = 8
 
 for i in range(nsim):
     if i == 0:
-        samples = numpy.array([brownian_motion(Δt, npts)])
+        samples = numpy.array([brownian_motion.brownian_motion(Δt, npts)])
     else:
-        samples = numpy.append(samples, numpy.array([brownian_motion(Δt, npts)]), axis=0)
+        samples = numpy.append(samples, numpy.array([brownian_motion.brownian_motion(Δt, npts)]), axis=0)
 time = numpy.linspace(0.0, float(npts-1)*Δt, npts)
 title = f"Brownian Motion; Δt={Δt}"
-multiplot(samples, time, [5.0, 12.0], title, "brownian_motion_1")
+brownian_motion.multiplot(samples, time, [5.0, 12.0], title, "brownian_motion_1")
 
 # %%
 
@@ -80,21 +33,21 @@ nsim = 1000
 
 for i in range(nsim):
     if i == 0:
-        samples = numpy.array([brownian_motion(Δt, npts)])
+        samples = numpy.array([brownian_motion.brownian_motion(Δt, npts)])
     else:
-        samples = numpy.append(samples, numpy.array([brownian_motion(Δt, npts)]), axis=0)
+        samples = numpy.append(samples, numpy.array([brownian_motion.brownian_motion(Δt, npts)]), axis=0)
 time = numpy.linspace(0.0, float(npts-1)*Δt, npts)
 title = f"Brownian Motion; Δt={Δt}"
-multiplot(samples, time, [5.0, 25.0], title, "brownian_motion_2")
+brownian_motion.multiplot(samples, time, [5.0, 25.0], title, "brownian_motion_2")
 
 # %%
 
 Δt = 0.01
 npts = 10000
-samples = brownian_motion(Δt, npts)
+samples = brownian_motion.brownian_motion(Δt, npts)
 time = numpy.linspace(0.0, float(npts-1)*Δt, npts)
 title = f"Brownian Motion; Δt={Δt}"
-plot(samples, time, title, "brownian_motion_3")
+brownian_motion.plot(samples, time, title, "brownian_motion_3")
 
 # %%
 
@@ -106,12 +59,12 @@ nsim = 8
 
 for i in range(nsim):
     if i == 0:
-        samples = numpy.array([brownian_motion_with_drift(μ, σ, Δt, npts)])
+        samples = numpy.array([brownian_motion.brownian_motion_with_drift(μ, σ, Δt, npts)])
     else:
-        samples = numpy.append(samples, numpy.array([brownian_motion_with_drift(μ, σ, Δt, npts)]), axis=0)
+        samples = numpy.append(samples, numpy.array([brownian_motion.brownian_motion_with_drift(μ, σ, Δt, npts)]), axis=0)
 time = numpy.linspace(0.0, float(npts-1)*Δt, npts)
 title = f"Brownian Motion with Drift; Δt={Δt}, μ={μ}, σ={σ}"
-multiplot(samples, time, [5.0, 8.0], title, "brownian_motion_with_drift_1")
+brownian_motion.multiplot(samples, time, [5.0, 8.0], title, "brownian_motion_with_drift_1")
 
 # %%
 
@@ -123,12 +76,12 @@ nsim = 1000
 
 for i in range(nsim):
     if i == 0:
-        samples = numpy.array([brownian_motion_with_drift(μ, σ, Δt, npts)])
+        samples = numpy.array([brownian_motion.brownian_motion_with_drift(μ, σ, Δt, npts)])
     else:
-        samples = numpy.append(samples, numpy.array([brownian_motion_with_drift(μ, σ, Δt, npts)]), axis=0)
+        samples = numpy.append(samples, numpy.array([brownian_motion.brownian_motion_with_drift(μ, σ, Δt, npts)]), axis=0)
 time = numpy.linspace(0.0, float(npts-1)*Δt, npts)
 title = f"Brownian Motion with Drift; Δt={Δt}, μ={μ}, σ={σ}"
-multiplot(samples, time, [5.0, 8.0], title, "brownian_motion_with_drift_2")
+brownian_motion.multiplot(samples, time, [5.0, 8.0], title, "brownian_motion_with_drift_2")
 
 # %%
 
@@ -141,12 +94,12 @@ s0 = 1.0
 
 for i in range(nsim):
     if i == 0:
-        samples = numpy.array([geometric_brownian_motion(μ, σ, s0, Δt, npts)])
+        samples = numpy.array([brownian_motion.geometric_brownian_motion(μ, σ, s0, Δt, npts)])
     else:
-        samples = numpy.append(samples, numpy.array([geometric_brownian_motion(μ, σ, s0, Δt, npts)]), axis=0)
+        samples = numpy.append(samples, numpy.array([brownian_motion.geometric_brownian_motion(μ, σ, s0, Δt, npts)]), axis=0)
 time = numpy.linspace(0.0, float(npts-1)*Δt, npts)
 title = f"Geometric Brownian Motion; Δt={Δt}, μ={μ}, σ={σ}, "+r"$S_0$" + f"={s0}"
-multiplot(samples, time, [5.0, 60.0], title, "geometric_brownian_motion_1")
+brownian_motion.multiplot(samples, time, [5.0, 60.0], title, "geometric_brownian_motion_1")
 
 
 # %%
@@ -160,27 +113,27 @@ s0 = 1.0
 
 for i in range(nsim):
     if i == 0:
-        samples = numpy.array([geometric_brownian_motion(μ, σ, s0, Δt, npts)])
+        samples = numpy.array([brownian_motion.geometric_brownian_motion(μ, σ, s0, Δt, npts)])
     else:
-        samples = numpy.append(samples, numpy.array([geometric_brownian_motion(μ, σ, s0, Δt, npts)]), axis=0)
+        samples = numpy.append(samples, numpy.array([brownian_motion.geometric_brownian_motion(μ, σ, s0, Δt, npts)]), axis=0)
 time = numpy.linspace(0.0, float(npts-1)*Δt, npts)
 title = f"Geometric Brownian Motion; Δt={Δt}, μ={μ}, σ={σ}, "+r"$S_0$" + f"={s0}"
-multiplot(samples, time, [5.0, 1000.0], title, "geometric_brownian_motion_2")
+brownian_motion.multiplot(samples, time, [5.0, 1000.0], title, "geometric_brownian_motion_2")
 
 # %%
 
 Δt = 0.01
-npts = 10000
-nsim = 10000
-μ = 0.025
-σ = 0.15
+npts = 100
+nsim = 5000
+μ = 0.0
+σ = 1.0
 s0 = 1.0
 
 for i in range(nsim):
     if i == 0:
-        samples = numpy.array([geometric_brownian_motion(μ, σ, s0, Δt, npts)])
+        samples = numpy.array([brownian_motion.geometric_brownian_motion(μ, σ, s0, Δt, npts)])
     else:
-        samples = numpy.append(samples, numpy.array([geometric_brownian_motion(μ, σ, s0, Δt, npts)]), axis=0)
+        samples = numpy.append(samples, numpy.array([brownian_motion.geometric_brownian_motion(μ, σ, s0, Δt, npts)]), axis=0)
 time = numpy.linspace(0.0, float(npts-1)*Δt, npts)
 title = f"Geometric Brownian Motion; Δt={Δt}, μ={μ}, σ={σ}, "+r"$S_0$" + f"={s0}"
-multiplot(samples, time, [5.0, 1000.0], title, "geometric_brownian_motion_3")
+brownian_motion.multiplot(samples, time, [0.2, 30.0], title, "geometric_brownian_motion_3")
---
+++ lib/brownian_motion.py
@@ -0,0 +1,50 @@
+import numpy
+from matplotlib import pyplot
+from lib import config
+
+pyplot.style.use(config.glyfish_style)
+
+# Brownian Motion Simulations
+
+def brownian_motion(Δt, n):
+    σ = numpy.sqrt(Δt)
+    samples = numpy.zeros(n)
+    for i in range(1, n):
+        Δ = numpy.random.normal()
+        samples[i] = samples[i-1] + σ * Δ
+    return samples
+
+def brownian_motion_with_drift(μ, σ, Δt, n):
+    samples = numpy.zeros(n)
+    for i in range(1, n):
+        Δ = numpy.random.normal()
+        samples[i] = samples[i-1] + (σ * Δ * numpy.sqrt(Δt)) + (μ * Δt)
+    return samples
+
+def geometric_brownian_motion(μ, σ, s0, Δt, n):
+    samples = brownian_motion_with_drift(μ, σ, Δt, n)
+    return s0*numpy.exp(samples)
+
+# Plots
+
+def multiplot(samples, time, text_pos, title, plot_name):
+    nplot = len(samples)
+    figure, axis = pyplot.subplots(figsize=(12, 8))
+    axis.set_xlabel("Time")
+    axis.set_ylabel("Value")
+    axis.set_title(title)
+    stats=f"Simulation Stats\n\nμ={format(numpy.mean(samples[:,-1]), '2.2f')}\nσ={format(numpy.std(samples[:,-1]), '2.2f')}"
+    bbox = dict(boxstyle='square,pad=1', facecolor="#FEFCEC", edgecolor="#FEFCEC", alpha=0.75)
+    axis.text(text_pos[0], text_pos[1], stats, fontsize=15, bbox=bbox)
+    for i in range(nplot):
+        axis.plot(time, samples[i], lw=1)
+    config.save_post_asset(figure, "mean_reversion", plot_name)
+
+def plot(samples, time, title, plot_name):
+    nplot = len(samples)
+    figure, axis = pyplot.subplots(figsize=(12, 8))
+    axis.set_xlabel("Time")
+    axis.set_ylabel("Value")
+    axis.set_title(title)
+    axis.plot(time, samples, lw=1)
+    config.save_post_asset(figure, "mean_reversion", plot_name)
