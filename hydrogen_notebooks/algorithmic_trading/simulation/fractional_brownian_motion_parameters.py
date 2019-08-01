d7c4e0283ac7dbfb4c30feefd0e923e4f39a8659
Troy Stribling <troy.stribling@gmail.com>
2019-08-01 08:41:32 -0700

    more work on fbm paramter plots

--- hydrogen_notebooks/algorithmic_trading/simulation/fractional_brownian_motion_parameters.py
+++ hydrogen_notebooks/algorithmic_trading/simulation/fractional_brownian_motion_parameters.py
@@ -14,32 +14,65 @@ pyplot.style.use(config.glyfish_style)
 
 # %%
 
-def variance(H, tmax, Δt):
-    nsteps = int(tmax/Δt)
-    time = numpy.linspace(0.0, tmax, nsteps)
+def fbm_variance(H, time):
     return time**(2.0*H)
 
-def autcorrelation(H, s, tmax, Δt):
-    nsteps = int(tmax/Δt)
-    time = numpy.linspace(0.0, tmax, steps)
-    return 0.5*(time**(2.0*H) + s**(2.0*H) + (time - s)**(2.0*H))
+def fbm_autocorrelation(H, s, time):
+    return 0.5*(time**(2.0*H) + s**(2.0*H) - numpy.abs(time - s)**(2.0*H))
+
+def fbm_autocorrelation_plot(H, s, time, file_name):
+    figure, axis = pyplot.subplots(figsize=(12, 8))
+    axis.set_xlabel("Time")
+    axis.set_ylabel("Value")
+    axis.set_title(r"Fraction Brownian Motion Autocorrelation, $\frac{1}{2}[t^{2H} + s^{2H} - \mid t-s \mid^{2H}]$" + f", s={format(s, '1.2f')}")
+
+    for H in H_vals:
+        axis.plot(time, fbm_autocorrelation(H, s, time), label=f"H={format(H, '1.2f')}")
+
+    axis.legend(ncol=2, bbox_to_anchor=(0.6, 0.35))
+    config.save_post_asset(figure, "brownian_motion", file_name)
 
 # %%
 
-tmax = 10.0
-Δt = 0.1
+tmax = 1.0
+Δt = 0.0001
 nsteps = int(tmax/Δt)
 
-H_vals = numpy.linspace(0.05, 1.0, 20)
+H_vals = [0.05, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1.0]
+time = numpy.linspace(0.0, tmax, nsteps)
+
+# %%
 
 figure, axis = pyplot.subplots(figsize=(12, 8))
 axis.set_xlabel("Time")
 axis.set_ylabel("Value")
-axis.set_title("Fraction Brownian Motion Variance")
+axis.set_title(r"Fraction Brownian Motion Variance, $t^{2H}$")
 
 for H in H_vals:
-    time = numpy.linspace(0.0, tmax, nsteps)
-    variance = variance(H, tmax, Δt)
-    axis.plot(time, variance)
+    axis.plot(time, fbm_variance(H, time), label=f"H={format(H, '1.2f')}")
 
+axis.legend(ncol=2, bbox_to_anchor=(0.6, 0.35))
 config.save_post_asset(figure, "brownian_motion", "fractional_brownian_motion_variance")
+
+# %%
+
+s = 0.5
+fbm_autocorrelation_plot(H, s, time, "fractional_brownian_motion_autocorrelation_0.5")
+
+# %%
+
+s = 1.0
+fbm_autocorrelation_plot(H, s, time, "fractional_brownian_motion_autocorrelation_1.0")
+
+
+# %%
+
+s = 1.0
+tmax = 1000.0
+Δt = 0.01
+nsteps = int(tmax/Δt)
+time = numpy.linspace(0.0, tmax, nsteps)
+
+# %%
+
+fbm_autocorrelation_plot(H, s, time, "fractional_brownian_motion_autocorrelation_long_time_1.0")
