b30c70a5e89151a191b208dd950e5c1f205d6d1a
Troy Stribling <troy.stribling@gmail.com>
2019-10-22 08:56:01 -0700

    started denominator integral simulations

--- hydrogen_notebooks/algorithmic_trading/regression/dickey_fuller_distribution.py
+++ hydrogen_notebooks/algorithmic_trading/regression/dickey_fuller_distribution.py
@@ -31,7 +31,7 @@ def modified_chi_squared(x):
     return  2.0*numpy.exp(-(2.0*x+1.0)/2.0) / numpy.sqrt(2.0*numpy.pi*(2.0*x+1.0))
 
 # stochastic integral simulation
-# int_0^1{B(s)dB(s)}
+# \int_0^1{B(s)dB(s)}
 
 def stochastic_integral_ensemble_1(n, nsample):
     vals = numpy.zeros(nsample)
@@ -46,15 +46,29 @@ def stochastic_integral_simulation_1(bn):
         val += brownian_motion(bn, i-1)*bn[i]
     return val
 
+# Analytic Solution of integral 1
+# \frac{1}{2}[B^2(1) - 1]
+
 def stochastic_integral_solution_1(n):
     return 0.5*(numpy.random.normal(0.0, 1.0, n)**2 - 1.0)
+
+# stochastic integral simulation
+# \sqrt{\int_0^1{B^2(s)ds}}
+
+def stochastic_integral_ensemble_2(n, nsample):
+    vals = numpy.zeros(nsample)
+    for i in range(nsample):
+        vals[i] = stochastic_integral_simulation_2(scaled_brownian_noise(n))
+    return vals
 
 def stochastic_integral_simulation_2(bn):
     n = len(bn)
     val = 0.0
     for i in range(1, n):
         val += brownian_motion(bn, i-1)**2
-    return val / n**2
+    return numpy.sqrt(val/n)
+
+# Plots
 
 def noise_plot(samples, time, plot_name):
     nsamples = len(samples)
@@ -64,7 +78,7 @@ def noise_plot(samples, time, plot_name):
     axis.plot(time, samples, lw=1)
     config.save_post_asset(figure, "regression", plot_name)
 
-def sample_plot(pdf, samples, title, plot, xrange=None, ylimit=None):
+def distribution_comparison_plot(pdf, samples, title, plot, xrange=None, ylimit=None):
     figure, axis = pyplot.subplots(figsize=(10, 7))
     axis.set_ylabel(r"$f_X(x)$")
     axis.set_xlabel(r"x")
@@ -82,6 +96,21 @@ def sample_plot(pdf, samples, title, plot, xrange=None, ylimit=None):
     axis.legend(bbox_to_anchor=(0.75, 0.9))
     config.save_post_asset(figure, "regression", plot)
 
+def distribution_plot(samples, title, plot, xrange=None, ylimit=None):
+    figure, axis = pyplot.subplots(figsize=(10, 7))
+    axis.set_ylabel(r"$f_X(x)$")
+    axis.set_xlabel(r"x")
+    axis.set_title(title)
+    axis.set_prop_cycle(config.distribution_sample_cycler)
+    _, bins, _ = axis.hist(samples, 50, rwidth=0.8, density=True, label=f"Samples", zorder=5)
+    if xrange is None:
+        delta = (bins[-1] - bins[0]) / 500.0
+        xrange = numpy.arange(bins[0], bins[-1], delta)
+    axis.set_xlim([xrange[0], xrange[-1]])
+    if ylimit is not None:
+        axis.set_ylim(ylimit)
+    config.save_post_asset(figure, "regression", plot)
+
 # %%
 
 n = 1000
@@ -94,7 +123,7 @@ noise_plot(bn, time, f"scaled_brownian_noise_{n}")
 
 # %%
 
-nsample = 100000
+nsample = 10000
 integral_samples = stochastic_integral_ensemble_1(n, nsample)
 integral_solution_samples = stochastic_integral_solution_1(nsample)
 
@@ -104,7 +133,7 @@ mean = numpy.mean(integral_solution_samples)
 sigma = numpy.sqrt(numpy.var(integral_solution_samples))
 title = r"$\frac{1}{2}[B^2(1) - 1]$, " + f"Sample Size={nsample}, μ={format(mean, '1.2f')}, σ={format(sigma, '1.2f')}"
 plot_name = f"stochastic_integral_simulation_1_solution_{nsample}"
-sample_plot(modified_chi_squared, integral_solution_samples, title, plot_name, numpy.arange(-0.49, 3.0, 0.01))
+distribution_comparison_plot(modified_chi_squared, integral_solution_samples, title, plot_name, numpy.arange(-0.49, 3.0, 0.01))
 
 
 # %%
@@ -113,7 +142,7 @@ mean = numpy.mean(integral_samples)
 sigma = numpy.sqrt(numpy.var(integral_samples))
 title = r"$\int_{0}^{1}B(s)dB(s)$, " + f"Sample Size={nsample}, T={n}, μ={format(mean, '1.2f')}, σ={format(sigma, '1.2f')}"
 plot_name = f"stochastic_integral_simulation_1_{nsample}"
-sample_plot(modified_chi_squared, integral_samples, title, plot_name, numpy.arange(-0.49, 3.0, 0.01))
+distribution_comparison_plot(modified_chi_squared, integral_samples, title, plot_name, numpy.arange(-0.49, 3.0, 0.01))
 
 # %%
 
@@ -127,7 +156,7 @@ mean = numpy.mean(integral_solution_samples)
 sigma = numpy.sqrt(numpy.var(integral_solution_samples))
 title = r"$\frac{1}{2}[B^2(1) - 1]$, " + f"Sample Size={nsample}, μ={format(mean, '1.2f')}, σ={format(sigma, '1.2f')}"
 plot_name = f"stochastic_integral_simulation_1_solution_{nsample}"
-sample_plot(modified_chi_squared, integral_solution_samples, title, plot_name, numpy.arange(-0.49, 3.0, 0.01))
+distribution_comparison_plot(modified_chi_squared, integral_solution_samples, title, plot_name, numpy.arange(-0.49, 3.0, 0.01))
 
 
 # %%
@@ -136,4 +165,18 @@ mean = numpy.mean(integral_samples)
 sigma = numpy.sqrt(numpy.var(integral_samples))
 title = r"$\int_{0}^{1}B(s)dB(s)$, " + f"Sample Size={nsample}, T={n}, μ={format(mean, '1.2f')}, σ={format(sigma, '1.2f')}"
 plot_name = f"stochastic_integral_simulation_1_{nsample}"
-sample_plot(modified_chi_squared, integral_samples, title, plot_name, numpy.arange(-0.49, 3.0, 0.01))
+distribution_comparison_plot(modified_chi_squared, integral_samples, title, plot_name, numpy.arange(-0.49, 3.0, 0.01))
+
+# %%
+
+n = 1000
+nsample = 1000
+integral_samples = stochastic_integral_ensemble_2(n, nsample)
+
+# %%
+
+mean = numpy.mean(integral_samples)
+sigma = numpy.sqrt(numpy.var(integral_samples))
+title = r"$\sqrt{\int_{0}^{1}B^2(s)ds}$, " + f"Sample Size={nsample}, T={n}, μ={format(mean, '1.2f')}, σ={format(sigma, '1.2f')}"
+plot_name = f"stochastic_integral_simulation_1_{nsample}"
+distribution_plot(integral_samples, title, plot_name)
