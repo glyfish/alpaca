017c54e7144d18f41dfd6ecf28b67015b14420ca
Troy Stribling <troy.stribling@gmail.com>
2019-11-09 14:25:01 -0800

    finshed f-test examples

--- hydrogen_notebooks/algorithmic_trading/regression/student_t_test.py
+++ hydrogen_notebooks/algorithmic_trading/regression/student_t_test.py
@@ -216,7 +216,7 @@ reg.cumvar(t)
 reg.cumulative_var_plot(t, var, title, plot, legend_pos=[0.7, 0.5])
 
 # %%
-# Latte Problem
+# Latte Problem (Single Sample Test)
 
 μ=4.0
 x_bar=4.6
@@ -227,6 +227,10 @@ df=n-1
 t = numpy.sqrt(n)*(x_bar-μ)/s
 t
 reg.student_t_tail(df)(2.064)
+reg.student_t_tail(df)(-2.064)
+reg.student_t_tail(df)(t)
+reg.student_t_tail(df)(1.71)
+reg.student_t_tail(df)(-1.71)
 
 # %%
 
@@ -235,3 +239,33 @@ ylabel = r"$f(t;3)$"
 xlabel = r"$t$"
 cdf = [reg.student_t_tail(df)(i) for i in x]
 reg.distribution_plot(cdf, x, title, ylabel, xlabel, "student_t_test_example_1_tail_cdf")
+
+
+# %%
+# Boise-LA Salaries (Paired Two Sample Test)
+
+boise_salaries = numpy.array([53047.0, 49958.0, 41974.0, 44366.0, 40470.0, 36963.0])
+la_salaries = numpy.array([62490.0, 58850.0, 49445.0, 52263.0, 47674.0, 43542.0])
+n = len(la_salaries)
+
+df= n - 1
+d = la_salaries - boise_salaries
+d
+numpy.mean(d)
+numpy.sqrt(reg.bias_corrected_var(d))
+t = numpy.sqrt(n)*numpy.mean(d) / numpy.sqrt(reg.bias_corrected_var(d))
+t
+
+reg.student_t_tail(df)(2.571)
+reg.student_t_tail(df)(-2.571)
+reg.student_t_tail(df)(t)
+
+reg.student_t_tail(df)(2.01)
+
+# %%
+
+title = f"Student-t Tail CDF, Number of Degrees of Freedom: {df}"
+ylabel = r"$f(t;3)$"
+xlabel = r"$t$"
+cdf = [reg.student_t_tail(df)(i) for i in x]
+reg.distribution_plot(cdf, x, title, ylabel, xlabel, "student_t_test_example_2_tail_cdf")
