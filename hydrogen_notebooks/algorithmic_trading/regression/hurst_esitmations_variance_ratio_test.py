24fc0bfa0a4760abf67f84b722a3c2f165213944
Troy Stribling <troy.stribling@gmail.com>
2019-12-15 12:51:10 -0800

    finished varainace ratio plots

--- hydrogen_notebooks/algorithmic_trading/regression/hurst_esitmations_variance_ratio_test.py
+++ hydrogen_notebooks/algorithmic_trading/regression/hurst_esitmations_variance_ratio_test.py
@@ -5,12 +5,13 @@
 import os
 import sys
 import numpy
+import scipy
 from matplotlib import pyplot
 from lib import config
 from lib import brownian_motion as bm
 from lib import regression as reg
 from lib import stats
-import statsmodels.api as sm
+
 
 wd = os.getcwd()
 yahoo_root = os.path.join(wd, 'data', 'yahoo')
@@ -41,24 +42,42 @@ def vr_plot(s_var, s, h, title, plot_name):
     axis.set_ylabel(r"Variance")
     axis.set_xlabel(r"$s$")
     axis.set_title(title)
-    axis.loglog(s_var, s, label=r"$\sigma^2(s)$", marker='o', markersize=5.0, linestyle="None", markeredgewidth=1.0, alpha=0.75)
-    axis.loglog(var, s, label=r"$s^{2H}$")
+    axis.loglog(s, s_var, label=r"$\sigma^2(s)$", marker='o', markersize=5.0, zorder=10, linestyle="None", markeredgewidth=1.0, alpha=0.75)
+    axis.loglog(s, var, label=r"$s^{2H}$", zorder=5)
     axis.legend(bbox_to_anchor=[0.8, 0.8])
     config.save_post_asset(figure, "regression", plot_name)
 
-def plot(sample, t, title, plot_name):
+def plot(samples, t, title, plot_name):
     figure, axis = pyplot.subplots(figsize=(12, 8))
     axis.set_ylabel(r"$x_t$")
     axis.set_xlabel(r"t")
     axis.set_title(title)
-    axis.plot(t, sample, lw=1.0)
+    axis.plot(t, samples, lw=1.0)
+    config.save_post_asset(figure, "regression", plot_name)
+
+def vr_test_plot(samples, s, sig_level, title, plot_name):
+    x_vals = numpy.linspace(-8.0, 8.0, 100)
+    y_vals = [scipy.stats.norm.cdf(x, 0.0, 1.0) for x in x_vals]
+    left_critical_value = scipy.stats.norm.ppf(sig_level/2.0, 0.0, 1.0)
+    right_critical_value = -left_critical_value
+    vr_test_stat = vr_statistic(samples, s)
+    figure, axis = pyplot.subplots(figsize=(12, 8))
+    axis.set_ylabel(r"$CDF$")
+    axis.set_xlabel(r"s")
+    axis.set_title(title)
+    axis.set_ylim([0.0, 1.0])
+    axis.plot(x_vals, y_vals)
+    axis.plot([left_critical_value, left_critical_value], [0.0, 1.0], color='red', label="Left Critical Value")
+    axis.plot([right_critical_value, right_critical_value], [0.0, 1.0], color='black', label="Right Critical Value")
+    axis.plot([vr_test_stat, vr_test_stat], [0.0, 1.0], color='green', label="t-Statistic")
+    axis.legend()
     config.save_post_asset(figure, "regression", plot_name)
 
 # %%
 
-npts = 100
+npts = 50
 fs = numpy.logspace(numpy.log10(10.0), numpy.log10(1000.0), npts)
-s_vals = list(dict.fromkeys([int(v) for v in fs]))
+s_vals = numpy.array(list(dict.fromkeys([int(v) for v in fs])))
 
 # %%
 
@@ -71,7 +90,138 @@ time = numpy.linspace(0.0, Δt*npts - 1, npts)
 # %%
 
 title = f"Fractional Brownian Motion: Δt={Δt}, H={H}"
-plot_name =f"variance_ration_fbm_H_{H}"
+plot_name =f"variance_ratio_fbm_H_{H}"
 plot(samples, time, title, plot_name)
 
 # %%
+
+s_var = numpy.array([s_period_variance(samples, s) for s in s_vals])
+
+# %%
+
+title = f"Variance Ratio: Δt={Δt}, H={H}"
+plot_name =f"variance_ratio_H_{H}"
+vr_plot(s_var, s_vals, H, title, plot_name)
+
+# %%
+
+H = 0.8
+Δt = 1.0
+npts = 2**16
+samples = bm.fbm_fft(H, Δt, npts)
+time = numpy.linspace(0.0, Δt*npts - 1, npts)
+
+# %%
+
+title = f"Fractional Brownian Motion: Δt={Δt}, H={H}"
+plot_name =f"variance_ratio_fbm_H_{H}"
+plot(samples, time, title, plot_name)
+
+# %%
+
+s_var = numpy.array([s_period_variance(samples, s) for s in s_vals])
+
+# %%
+
+title = f"Variance Ratio: Δt={Δt}, H={H}"
+plot_name =f"variance_ratio_H_{H}"
+vr_plot(s_var, s_vals, H, title, plot_name)
+
+# %%
+
+H = 0.4
+Δt = 1.0
+npts = 2**16
+samples = bm.fbm_fft(H, Δt, npts)
+time = numpy.linspace(0.0, Δt*npts - 1, npts)
+
+# %%
+
+title = f"Fractional Brownian Motion: Δt={Δt}, H={H}"
+plot_name =f"variance_ratio_fbm_H_{H}"
+plot(samples, time, title, plot_name)
+
+# %%
+
+s_var = numpy.array([s_period_variance(samples, s) for s in s_vals])
+
+# %%
+
+title = f"Variance Ratio: Δt={Δt}, H={H}"
+plot_name =f"variance_ratio_H_{H}"
+vr_plot(s_var, s_vals, H, title, plot_name)
+
+# %%
+
+H = 0.5
+Δt = 1.0
+npts = 2**16
+samples = bm.fbm_fft(H, Δt, npts)
+time = numpy.linspace(0.0, Δt*npts - 1, npts)
+α = 0.05
+
+# %%
+
+title = f"Variance Ratio Test: Δt={Δt}, H={H}, α={α}"
+plot_name =f"variance_ratio_test_H_{H}"
+vr_test_plot(samples, 1000, α, title, plot_name)
+
+ # %%
+
+H = 0.55
+Δt = 1.0
+npts = 2**16
+samples = bm.fbm_fft(H, Δt, npts)
+time = numpy.linspace(0.0, Δt*npts - 1, npts)
+α = 0.05
+
+# %%
+
+title = f"Variance Ratio Test: Δt={Δt}, H={H}, α={α}"
+plot_name =f"variance_ratio_test_H_{H}"
+vr_test_plot(samples, 1000, α, title, plot_name)
+
+# %%
+
+H = 0.4
+Δt = 1.0
+npts = 2**16
+samples = bm.fbm_fft(H, Δt, npts)
+time = numpy.linspace(0.0, Δt*npts - 1, npts)
+α = 0.05
+
+# %%
+
+title = f"Variance Ratio Test: Δt={Δt}, H={H}, α={α}"
+plot_name =f"variance_ratio_test_H_{H}"
+vr_test_plot(samples, 1000, α, title, plot_name)
+
+# %%
+
+H = 0.52
+Δt = 1.0
+npts = 2**16
+samples = bm.fbm_fft(H, Δt, npts)
+time = numpy.linspace(0.0, Δt*npts - 1, npts)
+α = 0.05
+
+# %%
+
+title = f"Variance Ratio Test: Δt={Δt}, H={H}, α={α}"
+plot_name =f"variance_ratio_test_H_{H}"
+vr_test_plot(samples, 1000, α, title, plot_name)
+
+# %%
+
+H = 0.48
+Δt = 1.0
+npts = 2**16
+samples = bm.fbm_fft(H, Δt, npts)
+time = numpy.linspace(0.0, Δt*npts - 1, npts)
+α = 0.05
+
+# %%
+
+title = f"Variance Ratio Test: Δt={Δt}, H={H}, α={α}"
+plot_name =f"variance_ratio_test_H_{H}"
+vr_test_plot(samples, 1000, α, title, plot_name)
