7cd0fd3d9887184c6b1ad7254943a33c3e20dd73
Troy Stribling <troy.stribling@gmail.com>
2020-03-15 15:47:17 -0700

    started work on moment comparison

--- hydrogen_notebooks/algorithmic_trading/mean_reversion/vector_autoregression_simulations.py
+++ hydrogen_notebooks/algorithmic_trading/mean_reversion/vector_autoregression_simulations.py
@@ -26,6 +26,9 @@ def timeseries_plot(samples, ylabel, title, plot_name):
     axis[nplot-1].set_xlabel(r"$t$")
     time = numpy.linspace(0, nsample-1, nsample)
     for i in range(nplot):
+        stats=f"μ={format(numpy.mean(samples[i]), '2.2f')}\nσ={format(numpy.std(samples[i]), '2.2f')}"
+        bbox = dict(boxstyle='square,pad=1', facecolor="#FEFCEC", edgecolor="#FEFCEC", alpha=0.75)
+        axis[i].text(0.05, 0.75, stats, fontsize=15, bbox=bbox, transform=axis[i].transAxes)
         axis[i].set_ylabel(ylabel[i])
         axis[i].set_ylim([ymin, ymax])
         axis[i].set_xlim([0.0, nsample])
@@ -62,7 +65,43 @@ def phi_companion_form(φ):
             else:
                 r = numpy.concatenate((r, numpy.zeros((n, n))), axis=1)
         p = numpy.concatenate((p, r), axis=0)
-    return p
+    return numpy.matrix(p)
+
+def mean_companion_form(μ):
+    n = len(μ)
+    p = numpy.zeros(n**2)
+    p[:n] = μ
+    return numpy.matrix([p]).T
+
+def omega_companion_form(Ω):
+    n, _ = Ω.shape
+    p = numpy.zeros((n**2, n**2))
+    p[:n, :n] = Ω
+    return numpy.matrix(p)
+
+def stationary_mean(Φ, Μ):
+    n, _ = Φ.shape
+    tmp = numpy.matrix(numpy.eye(n)) - Φ
+    return numpy.linalg.inv(tmp)*Μ
+
+def vec(m):
+    _, n = m.shape
+    v = numpy.matrix(numpy.zeros(n**2)).T
+    for i in range(n):
+        d = i*n
+        v[d:d+n] = m[:,i]
+    return v
+
+def unvec(v):
+    n2, _ = v.shape
+    n = int(numpy.sqrt(n2))
+    m = numpy.matrix(numpy.zeros((n, n)))
+    print(m.shape)
+    print(v.shape)
+    for i in range(n):
+        d = i*n
+        m[:,i] = v[d:d+n]
+    return m
 
 # %%
 
@@ -73,6 +112,41 @@ def phi_companion_form(φ):
                      [0.2, 0.1]])
 ])
 phi_companion_form(φ)
+
+# %%
+
+μ = [1.0, 2.0]
+mean_companion_form(μ)
+
+# %%
+
+Ω = numpy.matrix([[1.0, 0.5], [0.5, 2.0]])
+omega_companion_form(Ω)
+
+# %%
+
+m = numpy.matrix([[1,2,3],
+                  [4,5,6],
+                  [7,8,9]])
+vec(m)
+
+# %%
+
+unvec(vec(m))
+
+# %%
+
+μ = [1.0, 2.0]
+φ = numpy.array([
+        numpy.matrix([[1.0, 0.5],
+                     [0.5, 1.0]]),
+        numpy.matrix([[0.5, 0.3],
+                     [0.2, 0.1]])
+])
+
+Φ = phi_companion_form(φ)
+M = mean_companion_form(μ)
+stationary_mean(Φ,  M)
 
 # %%
 
@@ -92,16 +166,24 @@ phi_companion_form(φ)
 # %%
 
 μ = [0.0, 0.0]
-Ω = [[1.0, 0.0], [0.0, 1.0]]
+Ω = numpy.matrix([[1.0, 0.0], [0.0, 1.0]])
 φ = numpy.array([
-        numpy.matrix([[1.0, 0.0],
-                     [0.5, 0.0]]),
+        numpy.matrix([[0.5, 0.0],
+                     [0.0, 0.75]]),
         numpy.matrix([[0.0, 0.0],
                      [0.0, 0.0]])
 ])
 x0 = numpy.array([[0.0, 1.0], [0.0, 1.0]])
-n = 10
+n = 5000
 xt = var_simulate(x0, μ, φ, Ω, n)
+
+# %%
+# Diagonalize companion form of φ
+
+Φ = phi_companion_form(φ)
+λ, v = numpy.linalg.eig(Φ)
+v = numpy.matrix(v)
+numpy.linalg.inv(v)*Φ*v
 
 # %%
 
