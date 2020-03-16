8988e03b296fb27a417f68a526fe2ff0f8bd747d
Troy Stribling <troy.stribling@gmail.com>
2020-03-15 21:05:11 -0700

    mean nd varianace clculations seem to work

--- hydrogen_notebooks/algorithmic_trading/mean_reversion/vector_autoregression_simulations.py
+++ hydrogen_notebooks/algorithmic_trading/mean_reversion/vector_autoregression_simulations.py
@@ -73,16 +73,11 @@ def mean_companion_form(μ):
     p[:n] = μ
     return numpy.matrix([p]).T
 
-def omega_companion_form(Ω):
+def omega_companion_form(ω):
     n, _ = Ω.shape
     p = numpy.zeros((n**2, n**2))
-    p[:n, :n] = Ω
+    p[:n, :n] = ω
     return numpy.matrix(p)
-
-def stationary_mean(Φ, Μ):
-    n, _ = Φ.shape
-    tmp = numpy.matrix(numpy.eye(n)) - Φ
-    return numpy.linalg.inv(tmp)*Μ
 
 def vec(m):
     _, n = m.shape
@@ -96,12 +91,27 @@ def unvec(v):
     n2, _ = v.shape
     n = int(numpy.sqrt(n2))
     m = numpy.matrix(numpy.zeros((n, n)))
-    print(m.shape)
-    print(v.shape)
     for i in range(n):
         d = i*n
         m[:,i] = v[d:d+n]
     return m
+
+def stationary_mean(φ, μ):
+    Φ = phi_companion_form(φ)
+    Μ = mean_companion_form(μ)
+    n, _ = Φ.shape
+    tmp = numpy.matrix(numpy.eye(n)) - Φ
+    return numpy.linalg.inv(tmp)*Μ
+
+def stationary_covariance_matrix(φ, ω):
+    Ω = omega_companion_form(ω)
+    Φ = phi_companion_form(φ)
+    n, _ = Φ.shape
+    eye = numpy.matrix(numpy.eye(n**2))
+    tmp = eye - numpy.kron(Φ, Φ)
+    inv_tmp = numpy.linalg.inv(tmp)
+    vec_var = inv_tmp * vec(Ω)
+    return unvec(vec_var)
 
 # %%
 
@@ -110,6 +120,21 @@ def unvec(v):
                      [0.5, 1.0]]),
         numpy.matrix([[0.5, 0.3],
                      [0.2, 0.1]])
+])
+phi_companion_form(φ)
+
+# %%
+
+φ = numpy.array([
+        numpy.matrix([[1.0, 0.5, 2.0],
+                      [0.5, 1.0, 3.0],
+                      [0.5, 1.0, 3.0]]),
+        numpy.matrix([[2.0, 3.0, 4.0],
+                      [7.0, 6.0, 5.0],
+                      [8.0, 9.0, 10.0]]),
+        numpy.matrix([[7.0, 8.0, 9.0],
+                      [12.0, 11.0, 10.0],
+                      [13.0, 14.0, 15.0]])
 ])
 phi_companion_form(φ)
 
@@ -136,6 +161,18 @@ unvec(vec(m))
 
 # %%
 
+m = numpy.matrix([[1,2,3],
+                  [4,5,6],
+                  [7,8,9]])
+ones = numpy.matrix(numpy.ones((3,3)))
+numpy.kron(m, ones)
+
+# %%
+
+numpy.kron(ones, m)
+
+# %%
+
 μ = [1.0, 2.0]
 φ = numpy.array([
         numpy.matrix([[1.0, 0.5],
@@ -143,30 +180,24 @@ unvec(vec(m))
         numpy.matrix([[0.5, 0.3],
                      [0.2, 0.1]])
 ])
-
-Φ = phi_companion_form(φ)
-M = mean_companion_form(μ)
-stationary_mean(Φ,  M)
+stationary_mean(φ, μ)
 
 # %%
 
 φ = numpy.array([
-        numpy.matrix([[1.0, 0.5, 2.0],
-                      [0.5, 1.0, 3.0],
-                      [0.5, 1.0, 3.0]]),
-        numpy.matrix([[2.0, 3.0, 4.0],
-                      [7.0, 6.0, 5.0],
-                      [8.0, 9.0, 10.0]]),
-        numpy.matrix([[7.0, 8.0, 9.0],
-                      [12.0, 11.0, 10.0],
-                      [13.0, 14.0, 15.0]])
+        numpy.matrix([[0.5, 0.0],
+                     [0.0, 0.75]]),
+        numpy.matrix([[0.0, 0.0],
+                     [0.0, 0.0]])
 ])
 phi_companion_form(φ)
+ω = numpy.matrix([[1.0, 0.0], [0.0, 1.0]])
+stationary_covariance_matrix(φ, ω)
 
 # %%
 
 μ = [0.0, 0.0]
-Ω = numpy.matrix([[1.0, 0.0], [0.0, 1.0]])
+ω = numpy.matrix([[1.0, 0.0], [0.0, 1.0]])
 φ = numpy.array([
         numpy.matrix([[0.5, 0.0],
                      [0.0, 0.75]]),
@@ -175,7 +206,7 @@ phi_companion_form(φ)
 ])
 x0 = numpy.array([[0.0, 1.0], [0.0, 1.0]])
 n = 5000
-xt = var_simulate(x0, μ, φ, Ω, n)
+xt = var_simulate(x0, μ, φ, ω, n)
 
 # %%
 # Diagonalize companion form of φ
@@ -187,7 +218,40 @@ numpy.linalg.inv(v)*Φ*v
 
 # %%
 
+M = stationary_mean(φ, μ)
+Σ = stationary_covariance_matrix(φ, ω)
 plot_name = "var_2_simulation_1_x_y_timeseries"
-title = f"VAR(2) Simulation 1"
+title = f"VAR(2) Simulation: γ={format(Σ[0,1], '2.2f')}, " + \
+         r"$μ_x$=" + f"{format(M[0,0], '2.2f')}, " + \
+         r"$σ_x$=" + f"{format(numpy.sqrt(Σ[0,0]), '2.2f')}, " + \
+         r"$μ_y$=" + f"{format(M[1,0], '2.2f')}, " + \
+         r"$σ_y$=" + f"{format(numpy.sqrt(Σ[1,1]), '2.2f')}"
+ylabel = [r"$x$", r"$y$"]
+timeseries_plot(xt, ylabel, title, plot_name)
+
+# %%
+
+μ = [0.0, 0.0]
+ω = numpy.matrix([[1.0, 0.0], [0.0, 1.0]])
+φ = numpy.array([
+        numpy.matrix([[0.5, 0.25],
+                     [0.25, 0.75]]),
+        numpy.matrix([[0.0, 0.0],
+                     [0.0, 0.0]])
+])
+x0 = numpy.array([[0.0, 1.0], [0.0, 1.0]])
+n = 5000
+xt = var_simulate(x0, μ, φ, ω, n)
+
+# %%
+
+M = stationary_mean(φ, μ)
+Σ = stationary_covariance_matrix(φ, ω)
+plot_name = "var_2_simulation_2_x_y_timeseries"
+title = f"VAR(2) Simulation: γ={format(Σ[0,1], '2.2f')}, " + \
+         r"$μ_x$=" + f"{format(M[0,0], '2.2f')}, " + \
+         r"$σ_x$=" + f"{format(numpy.sqrt(Σ[0,0]), '2.2f')}, " + \
+         r"$μ_y$=" + f"{format(M[1,0], '2.2f')}, " + \
+         r"$σ_y$=" + f"{format(numpy.sqrt(Σ[1,1]), '2.2f')}"
 ylabel = [r"$x$", r"$y$"]
 timeseries_plot(xt, ylabel, title, plot_name)
