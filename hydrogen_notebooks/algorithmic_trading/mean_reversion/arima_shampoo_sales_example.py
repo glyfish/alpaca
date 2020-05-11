7c6c8ef5ea367778106b5f4a4ec264920d42b385
Troy Stribling <troy.stribling@gmail.com>
2020-05-11 09:03:00 -0700

    started shapoo sales example

--- hydrogen_notebooks/algorithmic_trading/mean_reversion/arima_shampoo_sales_example.py
+++ hydrogen_notebooks/algorithmic_trading/mean_reversion/arima_shampoo_sales_example.py
@@ -22,17 +22,16 @@ wd = os.getcwd()
 
 # %%
 
-def plot(df):
-    figure, axis = pyplot.subplots(nrows=4, ncols=2, figsize=(10, 8))
-    for i, axis in enumerate(axis.flatten()):
-        data = df[df.columns[i]]
-        axis.plot(data)
-        axis.set_title(df.columns[i], fontsize=12)
-        axis.tick_params(axis="x", labelsize=8)
-        axis.tick_params(axis="y", labelsize=8)
+def plot(df, title, plot_name):
+    figure, axis = pyplot.subplots(figsize=(10, 8))
+    figure.autofmt_xdate()
+    data = df[df.columns[0]]
+    axis.plot(data)
+    axis.set_title(df.columns[0])
+    axis.set_xlabel(df.index.name)
+    axis.set_ylabel(df.columns[0])
     pyplot.tight_layout(pad=1.0)
-    config.save_post_asset(figure, "mean_reversion", "machine_learning_plus_macro_example_timeseries")
-
+    config.save_post_asset(figure, "mean_reversion", plot_name)
 
 def date_parser(date):
     return pandas.datetime.strptime("201"+date, "%Y-%m")
@@ -41,3 +40,9 @@ def date_parser(date):
 
 filepath = os.path.join(wd, "data", "examples", "shampoo-sales.csv")
 df = pandas.read_csv(filepath, parse_dates=['Month'], index_col='Month', date_parser=date_parser)
+
+# %%
+
+title = "Shampoo Sales"
+plot_name = "arima_example_shampoo_sales"
+plot(df, title, plot_name)
