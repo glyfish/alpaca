3623c134df1acec402ca65da81a61d16fbbf48df
Troy Stribling <troy.stribling@gmail.com>
2020-10-13 20:04:53 -0700

    vecm simulator works

--- hydrogen_notebooks/algorithmic_trading/mean_reversion/vecm_simulation.py
+++ hydrogen_notebooks/algorithmic_trading/mean_reversion/vecm_simulation.py
@@ -20,6 +20,9 @@ pyplot.style.use(config.glyfish_style)
 
 # %%
 
+def multivariate_normal_sample(μ, Ω, n):
+    return numpy.random.multivariate_normal(μ, Ω, n)
+
 def comparison_plot(title, samples, labels, plot):
     nplot, nsamples = samples.shape
     figure, axis = pyplot.subplots(figsize=(10, 7))
@@ -27,15 +30,70 @@ def comparison_plot(title, samples, labels, plot):
     axis.set_xlabel(r"$t$")
     axis.set_xlim([0, nsamples-1])
     for i in range(nplot):
-        axis.plot(range(nsamples), samples[i], label=labels[i], lw=1)
+        axis.plot(range(nsamples), samples[i].T, label=labels[i], lw=1)
     axis.legend(fontsize=16)
     config.save_post_asset(figure, "mean_reversion", plot)
 
-def vecm_generate_sample(α, β, a, nsample):
+def vecm_generate_sample(α, β, a, Ω, nsample):
     n, _ = a.shape
-    xt = numpy.matrix(numpy.zeros(n, nsample))
+    xt = numpy.matrix(numpy.zeros((n, nsample)))
+    εt = numpy.matrix(multivariate_normal_sample(numpy.zeros(n), Ω, nsample))
     for i in range(2, nsample):
         Δxt1 = xt[:,i-1] - xt[:,i-2]
+        Δxt = α*β*xt[:,i-1] + a*Δxt1 + εt[i].T
+        xt[:,i] = Δxt + xt[:,i-1]
     return xt
 
 # %%
+
+nsample = 1000
+α = numpy.matrix([-0.5, 0.0, 0.0]).T
+β = numpy.matrix([1.0, -0.5, -0.5])
+a = numpy.matrix([[0.5, 0.0, 0.0],
+                  [0.0, 0.5, 0.0],
+                  [0.0, 0.0, 0.5]])
+Ω = numpy.matrix([[1.0, 0.0, 0.0],
+                  [0.0, 1.0, 0.0],
+                  [0.0, 0.0, 1.0]])
+
+title = "VECM"
+labels = [r"$x_1$", r"$x_2$", r"$x_3$"]
+plot = "vecm_comparison_1"
+samples = vecm_generate_sample(α, β, a, Ω, nsample)
+comparison_plot(title, samples, labels, plot)
+
+# %%
+
+nsample = 1000
+α = numpy.matrix([-0.5, 0.0, 0.0]).T
+β = numpy.matrix([1.0, -0.5, -0.0])
+a = numpy.matrix([[0.5, 0.0, 0.0],
+                  [0.0, 0.5, 0.0],
+                  [0.0, 0.0, 0.5]])
+Ω = numpy.matrix([[1.0, 0.0, 0.0],
+                  [0.0, 1.0, 0.0],
+                  [0.0, 0.0, 1.0]])
+
+title = "VECM"
+labels = [r"$x_1$", r"$x_2$", r"$x_3$"]
+plot = "vecm_comparison_1"
+samples = vecm_generate_sample(α, β, a, Ω, nsample)
+comparison_plot(title, samples, labels, plot)
+
+# %%
+
+nsample = 1000
+α = numpy.matrix([-0.5, 0.0, 0.0]).T
+β = numpy.matrix([1.0, 0.0, -0.5])
+a = numpy.matrix([[0.5, 0.5, 0.0],
+                  [0.0, 0.5, 0.0],
+                  [0.0, 0.0, 0.5]])
+Ω = numpy.matrix([[1.0, 0.0, 0.0],
+                  [0.0, 1.0, 0.0],
+                  [0.0, 0.0, 1.0]])
+
+title = "VECM"
+labels = [r"$x_1$", r"$x_2$", r"$x_3$"]
+plot = "vecm_comparison_1"
+samples = vecm_generate_sample(α, β, a, Ω, nsample)
+comparison_plot(title, samples, labels, plot)
