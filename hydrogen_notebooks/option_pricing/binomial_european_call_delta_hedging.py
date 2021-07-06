7b6cd3267f80a32068e514d456b40f71369fed8d
Troy Stribling <troy.stribling@gmail.com>
2021-07-06 11:51:59 -0700

    added delta hedging example

--- hydrogen_notebooks/option_pricing/binomial_european_call_delta_hedging.py
+++ hydrogen_notebooks/option_pricing/binomial_european_call_delta_hedging.py
@@ -41,6 +41,15 @@ def european_call_payoff(U, D, R, S0, K, n):
     Ψq1 = binomial_tail_cdf(l - 1, n, q1)
     return S0*Ψq1 - K*(1 + R)**(-n)*Ψq
 
+def delta(CU, CD, SU, SD):
+    return (CU - CD) / (SU - SD)
+
+def init_borrow(S0, C0, x):
+    return C0 - S0 * x
+
+def borrow(y, R, x1, x2, S):
+    return y * (1 + R) + (x1 - x2) * S
+
 # %%
 
 n = 3
@@ -64,16 +73,45 @@ binom.cdf(l, n, q)
 # %
 # t = 0
 
-european_call_payoff(U, D, R, S0, K, n)
+C0 = european_call_payoff(U, D, R, S0, K, n)
 
 # %%
 # Delta hedge
-# t = 1
+# t = 0
 
 S1U = S0*(1.0 + U)
 S1D = S0*(1.0 + D)
 
-european_call_payoff(U, D, R, S1U, K, n - 1)
-european_call_payoff(U, D, R, S1D, K, n - 1)
+C1U = european_call_payoff(U, D, R, S1U, K, n - 1)
+C1D = european_call_payoff(U, D, R, S1D, K, n - 1)
+
+x1 = delta(C1U, C1D, S1U, S1D)
+y1 = init_borrow(S0, C0, x1)
+
+# t = 1
+# The price goes up S1 = S0*(1+U)
+
+S1 = S0 * (1 + U)
+
+S2U = S1*(1.0 + U)
+S2D = S1*(1.0 + D)
+
+C2U = european_call_payoff(U, D, R, S2U, K, n - 2)
+C2D = european_call_payoff(U, D, R, S2D, K, n - 2)
+
+x2 = delta(C2U, C2D, S2U, S2D)
+y2 = borrow(y1, R, x1, x2, S1)
 
 # t = 2
+# The price goes down S1 = S0*(1+U)*(1+D)
+
+S2 = S0 * (1 + U) * (1 + D)
+
+S3U = S2*(1.0 + U)
+S3D = S2*(1.0 + D)
+
+C3U = european_call_payoff(U, D, R, S3U, K, n - 3)
+C3D = european_call_payoff(U, D, R, S3D, K, n - 3)
+
+x3 = delta(C3U, C3D, S3U, S3D)
+y3 = borrow(y2, R, x2, x3, S2)
