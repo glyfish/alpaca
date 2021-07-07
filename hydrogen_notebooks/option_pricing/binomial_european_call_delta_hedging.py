a15b4d3022b620a08a0c4963c9fe139e1d48d982
Troy Stribling <troy.stribling@gmail.com>
2021-07-07 08:19:24 -0700

    added portfolio calculation

--- hydrogen_notebooks/option_pricing/binomial_european_call_delta_hedging.py
+++ hydrogen_notebooks/option_pricing/binomial_european_call_delta_hedging.py
@@ -50,6 +50,9 @@ def init_borrow(S0, C0, x):
 def borrow(y, R, x1, x2, S):
     return y * (1 + R) + (x1 - x2) * S
 
+def portfolio_value(x, S, y):
+    return x * S + y
+
 # %%
 
 n = 3
@@ -88,6 +91,8 @@ C1D = european_call_payoff(U, D, R, S1D, K, n - 1)
 x1 = delta(C1U, C1D, S1U, S1D)
 y1 = init_borrow(S0, C0, x1)
 
+portfolio_value(x1, S0, y1)
+
 # t = 1
 # The price goes up S1 = S0*(1+U)
 
@@ -102,6 +107,8 @@ C2D = european_call_payoff(U, D, R, S2D, K, n - 2)
 x2 = delta(C2U, C2D, S2U, S2D)
 y2 = borrow(y1, R, x1, x2, S1)
 
+portfolio_value(x2, S1, y2)
+
 # t = 2
 # The price goes down S1 = S0*(1+U)*(1+D)
 
@@ -115,3 +122,5 @@ C3D = european_call_payoff(U, D, R, S3D, K, n - 3)
 
 x3 = delta(C3U, C3D, S3U, S3D)
 y3 = borrow(y2, R, x2, x3, S2)
+
+portfolio_value(x3, S2, y3)
